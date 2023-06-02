cc5b99eccf83b1333b6f23c56c3a3c579be9dbf2
Dex-zabeth <Dex-zabeth@users.noreply.github.com>
2023-06-01 22:05:02 -0400

    update landing

---
+++ navbar.js
@@ -0,0 +1,9 @@
+
+function toggleshow() {
+  var x = document.getElementById("topnav");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
