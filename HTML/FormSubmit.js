eaa0a6c5a8df4fabd12bd9091a122f6d3a06f612
Dex-zabeth <Dex-zabeth@users.noreply.github.com>
2023-06-11 18:22:40 -0400

    attempt ???

--- HTML/FormSubmit.js
+++ HTML/FormSubmit.js
@@ -1,63 +1,13 @@
-javascript
+const myForm = document.getElementById('myForm');
 const recipient = 'Joshua.Michalik@triosstudent.com';
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const sender = document.getElementById('sender').value;
+    const name = document.getElementById('name').value;
+    const subject = document.getElementById('subject').value;
+    const body = document.getElementById('body').value;
 
+    const mailtoLink = `mailto:${recipient}?subject=${subject}&amp;body=${body}&amp;cc=${sender}`;
 
- 
-
-window.location.href = mailtoLink;
-const nameEl = document.querySelector("#name");
-const emailEl = document.querySelector("#email");
-const companyNameEl = document.querySelector("#company-name");
-const messageEl = document.querySelector("#message");
-let valid = false;
-const form = document.querySelector("#submit-form");
-
-function checkValidations() {
-    let letters = /^[a-zA-Z\s]*$/;
-    const name = nameEl.value.trim();
-    const email = emailEl.value.trim();
-    const companyName = companyNameEl.value.trim();
-    const message = messageEl.value.trim();
-    if (name === "") {
-        document.querySelector(".name-error").classList.add("error");
-        document.querySelector(".name-error").innerText =
-            "Please fill out this field!";
-    } else {
-        if (!letters.test(name)) {
-            document.querySelector(".name-error").classList.add("error");
-            document.querySelector(".name-error").innerText =
-                "Please enter only characters!";
-        
-        } else {
-        
-        }
-    }
-    if (email === "") {
-        document.querySelector(".email-error").classList.add("error");
-        document.querySelector(".email-error").innerText =
-            "Please fill out this field!";
-        
-    } else {
-        if (!/^\S+@\S+\.\S+$/.test(email)) {
-            document.querySelector(".email-error").classList.add("error");
-            document.querySelector(".email-error").innerText =
-                "Please enter a valid email address!";
-          
-        } else {
-          
-        }
-    }
-  
-}
-
-function sendEmail(name, email, companyName, message) {
-    window.location.href = `mailto:${recipient}?subject=${subject}&body=${body}`;
-
-function reset() {
-    nameEl.value = "";
-    emailEl.value = "";
-    companyNameEl.value = "";
-    messageEl.value = "";
-    document.querySelector(".name-error").innerText = "";
-    document.querySelector(".email-error").innerText = "";
-}
+    window.location.href = mailtoLink;
+});
