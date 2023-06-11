3c7cfd4737e282d3a0615c847b684fde11b2ed88
Dex-zabeth <Dex-zabeth@users.noreply.github.com>
2023-06-11 18:13:52 -0400

    c

--- JavaScripts/FormSubmit.js
+++ JavaScripts/FormSubmit.js
@@ -1,34 +1,13 @@
-const form = document.querySelector('form');
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const messageInput = document.getElementById('message');
-const submitButton = document.querySelector('input[type="submit"]');
+const myForm = document.getElementById('myForm');
+const recipient = 'Joshua.Michalik@triosstudent.com';
+myForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const sender = document.getElementById('sender').value;
+    const name = document.getElementById('name').value;
+    const subject = document.getElementById('subject').value;
+    const body = document.getElementById('body').value;
 
-form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    const mailtoLink = `mailto:${recipient}?subject=${subject}&amp;body=${body}&amp;cc=${sender}`;
 
-    // Disable the submit button to prevent multiple submissions
-    submitButton.disabled = true;
-
-    // Get the form data
-    const name = nameInput.value;
-    const email = emailInput.value;
-    const message = messageInput.value;
-
-    // TODO: Add validation and error handling here
-
-    // Send the data using AJAX
-    const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'submit-form.php');
-    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-    xhr.onload = () => {
-        if (xhr.status === 200) {
-            // Redirect the user to a thank-you page
-            window.location.href = 'index.html';
-        } else {
-            // Display an error message
-            alert('There was an error submitting the form. Please try again later.');
-        }
-    };
-    xhr.send(`name=${name}&email=${email}&message=${message}`);
+    window.location.href = mailtoLink;
 });
